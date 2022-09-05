528b7c3bf8a803058e6f03474a63da2541f41f1c
NataliiaPasaman <natapasaman@gmail.com>
2022-09-05 22:18:04 +0300

    add setInterval()
    add setInterval() on btn Start and clear on btn Stop

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+/** Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
+
+УВАГА
+Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. 
+Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled). */
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+
+refs.startBtn.addEventListener('click', startColorSwitcger);
+refs.stopBtn.addEventListener('click', stopColorSwitcher);
+
+function startColorSwitcger() {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopColorSwitcher() {
+    clearInterval(timerId);
+}
