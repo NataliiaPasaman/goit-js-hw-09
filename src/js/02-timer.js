d895fb562822cfc2c38951e4ca6c6459a59e994d
NataliiaPasaman <natapasaman@gmail.com>
2022-09-10 18:56:09 +0300

    script-2 - valid and invalid date

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,14 +2,10 @@
 import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
-/** Напиши скрипт таймера, який здійснює зворотний відлік до певної дати. 
- * 
- * 
- */
 
-
-/** HTML містить готову розмітку таймера, поля вибору кінцевої дати і кнопку, по кліку на яку, 
- * таймер повинен запускатися. Додай мінімальне оформлення елементів інтерфейсу. */
+ const inputData = document.querySelector('#datetime-picker');
+ const btnStart = document.querySelector('[data-start]');
+ btnStart.setAttribute('disabled', 'true');
 
  const options = {
     enableTime: true,
@@ -21,4 +17,46 @@ import "flatpickr/dist/flatpickr.min.css";
     },
   };
 
-flatpickr("#datetime-picker", options);
+const fp = flatpickr(inputData, options);
+fp.selectedDates.push(fp._input.value);
+
+const currentData = new Date;
+
+inputData.addEventListener('input', onSelectedDatesInput)
+btnStart.addEventListener('click', onStartTimer);
+
+function onSelectedDatesInput() {
+
+  console.log('currentData', currentData);
+  let changedDates = fp.selectedDates[0]
+  console.log('changedDates', changedDates);
+
+  inputInvalidDate(changedDates);
+  inputValidDate(changedDates);
+}
+
+function onStartTimer() {
+
+}
+// onStartTimer();
+
+/** 
+
+Натисканням на кнопку «Start» починається відлік часу до обраної дати з моменту натискання.
+ */
+
+function inputInvalidDate(changedDates) {
+  setTimeout(() => {
+    if(currentData > changedDates) {
+      window.alert("Please choose a date in the future");
+    };
+  }, 2000);
+}
+
+function inputValidDate(changedDates) {
+  setTimeout(() => {
+    if(currentData > changedDates) {
+      btnStart.removeAttribute('disabled');
+    };
+  }, 2000);
+}
